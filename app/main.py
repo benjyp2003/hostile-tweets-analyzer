e8b698cf423642fc4b2c4a3b296000c288ca996d
benjyp2003 <benjypfeffer@gmail.com>
2025-08-21 16:30:08 +0300

    Created main file with endpoints:  /root for health checking  /get_analyzed_data for returning the processed data  and an 'onevent' that processes the data when the server starts

---
+++ app/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+import uvicorn
+
+from manager import Manager
+app = FastAPI()
+manager = Manager()
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Tweets Analyzer API, use the endpoint /get_analyzed_data to get the analyzed data."}
+
+
+@app.get("/get_analyzed_data")
+def get_analyzed_data():
+    return manager.get_processed_data()
+
+@app.on_event("startup")
+def startup_event():
+    """
+    This function is called when the application starts.
+    It processes the data and stores it in the manager.
+    """
+    manager.process_data()
+
+
